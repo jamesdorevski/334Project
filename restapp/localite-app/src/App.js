1d76d8f21c2f67140991d04e051dd59def68f112
andreaburazor <andreaburazor@gmail.com>
2020-05-24 13:20:33 +1000

    AB757 implements pass search params to backend to return tours

--- restapp/localite-app/src/App.js
+++ restapp/localite-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { Redirect } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom"
+import { Routes } from "./navigation/routes";
 import AuthenticatedRoute from "./components/AuthenticatedRoute"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
@@ -19,33 +19,35 @@ import PastToursComponent from "./components/PastToursComponent"
 import ProfileComponent from "./components/ProfileComponent"
 import LoginComponent from "./components/LoginComponent"
 
-class App extends Component {
-  render() {
+export const App = () => {
     return (
       <div className="App">
         <Router>
           <HeaderComponent />
           <Switch>
-            <Route path="/" exact component={HomeComponent} />
-            <Route path="/home" component={HomeComponent} />
-            <Route path="/login" component={LoginComponent} />
-            <Route path="/search" component={TourResultsComponent} />
-            <Route path="/signup/guide" component={BecomeAGuideComponent} />
-            <Route path="/signup" component={SignUpComponent} />
-            <Route path="/about" component={AboutUsComponent} />
-            <Route path="/faq" component={FAQComponent} />
-            <Route path="/disputes" component={DisputeResolutionComponent} />
-            <Route path="/account/show/:id" component={ProfileComponent} />
-            <AuthenticatedRoute path="/account/:id/upcoming" component={UpcomingToursComponent} />
-            <AuthenticatedRoute path="/account/:id/past" component={PastToursComponent} />
-            <AuthenticatedRoute path="/account/info" component={AccountComponent} />
-            <Route render={() => <Redirect to={{pathname: "/"}} />} />
+
+            {/*<Route render={() => <Redirect to={{pathname: "/"}} />} />*/}
+            <Route path={Routes.HOME} component={HomeComponent} />
+            <Route path={Routes.LOGIN} component={LoginComponent} />
+            <Route path={Routes.SEARCH} component={TourResultsComponent} />
+            <Route path={Routes.SIGN_UP_GUIDE} component={BecomeAGuideComponent} />
+            <Route path={Routes.SIGN_UP} component={SignUpComponent} />
+            <Route path={Routes.ABOUT} component={AboutUsComponent} />
+            <Route path={Routes.FAQ} component={FAQComponent} />
+            <Route path={Routes.DISPUTES} component={DisputeResolutionComponent} />
+            {/*<Route path={Routes.TOURS} component={TourComponent} />*/}
+            <Route path={Routes.PROFILE} component={ProfileComponent} />
+            <AuthenticatedRoute path={Routes.UPCOMING_TOURS} component={UpcomingToursComponent} />
+            <AuthenticatedRoute path={Routes.PAST_TOURS} component={PastToursComponent} />
+            <AuthenticatedRoute path={Routes.ACCOUNT} component={AccountComponent} />
+            <Route path="*">
+                <Redirect to={Routes.HOME} />
+            </Route>
           </Switch>
           <FooterComponent />
         </Router>
       </div>
     );
-  }
 }
 
 export default App;
---
+++ restapp/localite-app/src/navigation/routes.jsx
@@ -0,0 +1,32 @@
+/**
+ *  Enums representing the available URL routes in the application.
+ */
+
+export const Routes =
+    {
+        HOME: '/home',
+        LOGIN:
+            '/login',
+        SEARCH:
+            '/search',
+        SIGN_UP:
+            '/signup',
+        SIGN_UP_GUIDE:
+            '/signup/guide',
+        ABOUT:
+            '/about',
+        FAQ:
+            '/faq',
+        DISPUTES:
+            '/disputes',
+        TOURS:
+            '/tours/:id',
+        PROFILE:
+            '/account/show/:id',
+        UPCOMING_TOURS:
+            '/account/:id/upcoming',
+        PAST_TOURS:
+            '/account/:id/past',
+        ACCOUNT:
+            '/account/:id',
+    }
