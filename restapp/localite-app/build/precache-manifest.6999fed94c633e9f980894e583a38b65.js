4354e672de57811c71eb68dc0f692becbf7a0385
brittchin99 <brittchin1@outlook.com>
2020-05-31 14:10:14 -0400

    working with yarn

---
+++ restapp/localite-app/build/precache-manifest.6999fed94c633e9f980894e583a38b65.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fe8c79ab42f37154a2015be1feeb8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fbe0e008054fdb99d9c",
+    "url": "/static/css/2.01eea785.chunk.css"
+  },
+  {
+    "revision": "5dab47daffe170d6f04e",
+    "url": "/static/css/main.0a28d5a3.chunk.css"
+  },
+  {
+    "revision": "4fbe0e008054fdb99d9c",
+    "url": "/static/js/2.76f9e561.chunk.js"
+  },
+  {
+    "revision": "c7cfadd107f0515df831763e17d6b340",
+    "url": "/static/js/2.76f9e561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dab47daffe170d6f04e",
+    "url": "/static/js/main.65acc7b3.chunk.js"
+  },
+  {
+    "revision": "a14789c8e395aac01d09",
+    "url": "/static/js/runtime-main.25dd1063.js"
+  },
+  {
+    "revision": "67d32b5d15f167d8fb6b0b16237d331c",
+    "url": "/static/media/andrea.67d32b5d.jpg"
+  },
+  {
+    "revision": "a19c8ab9c6ca46467f352158a8141226",
+    "url": "/static/media/art.a19c8ab9.jpg"
+  },
+  {
+    "revision": "46dbb0e4ba1d1845a319a17ca764c26f",
+    "url": "/static/media/blue-mountains.46dbb0e4.jpg"
+  },
+  {
+    "revision": "ab50f37c61458f739061041105efb009",
+    "url": "/static/media/book.ab50f37c.png"
+  },
+  {
+    "revision": "1faf07c64146f773ca221e13ba361c79",
+    "url": "/static/media/brittney.1faf07c6.jpg"
+  },
+  {
+    "revision": "4995bf58314a049ba45c457a098d8b98",
+    "url": "/static/media/connect.4995bf58.png"
+  },
+  {
+    "revision": "11f8a3d579c503be06a1d0630eef8f4c",
+    "url": "/static/media/day.11f8a3d5.jpg"
+  },
+  {
+    "revision": "80d073f0a4fe3a6cc14f1429c795e27b",
+    "url": "/static/media/experience.80d073f0.png"
+  },
+  {
+    "revision": "0797ae0bb0546d00f5502a9109d423a1",
+    "url": "/static/media/food.0797ae0b.jpg"
+  },
+  {
+    "revision": "9c37dfdcc70d9ddc7fb8470429db0678",
+    "url": "/static/media/foodtour.9c37dfdc.jpg"
+  },
+  {
+    "revision": "d08fa4551086f11a121a0dfb0f16310c",
+    "url": "/static/media/guide.d08fa455.jpg"
+  },
+  {
+    "revision": "372efb995ef79131c81c3ee931d02435",
+    "url": "/static/media/hiking.372efb99.jpg"
+  },
+  {
+    "revision": "c387038fcbd74c554089f2ac6626b8c1",
+    "url": "/static/media/horses.c387038f.jpg"
+  },
+  {
+    "revision": "19e29ea2f787c1dace31d2505322d9b6",
+    "url": "/static/media/jackie.19e29ea2.jpg"
+  },
+  {
+    "revision": "06ca62f9f70dcecb05781a833c627f98",
+    "url": "/static/media/james.06ca62f9.jpg"
+  },
+  {
+    "revision": "b3959b3aa5fd23305bc878c7cef24802",
+    "url": "/static/media/josh.b3959b3a.jpg"
+  },
+  {
+    "revision": "d0477a978ad40a4016a0b7b3051118bf",
+    "url": "/static/media/kids.d0477a97.jpg"
+  },
+  {
+    "revision": "2221a4e306a22d5604aa4a3a7b26f5c0",
+    "url": "/static/media/leanne.2221a4e3.jpg"
+  },
+  {
+    "revision": "ad09661c37a0a45665048c2d4f254b9b",
+    "url": "/static/media/mountains.ad09661c.jpg"
+  },
+  {
+    "revision": "2b061298f2feb5f59789f0c2e62d3333",
+    "url": "/static/media/museum.2b061298.jpg"
+  },
+  {
+    "revision": "c17e92a80422bb5723feb8955d9b3b09",
+    "url": "/static/media/night.c17e92a8.jpg"
+  },
+  {
+    "revision": "1601b4d0dde5356364b20ac3795d42ed",
+    "url": "/static/media/plane.1601b4d0.jpg"
+  },
+  {
+    "revision": "f0ea02a291b1d793f4394ff1f53b5818",
+    "url": "/static/media/plane2.f0ea02a2.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "ed4af04e1f092e7361e6e211c1ab7f6e",
+    "url": "/static/media/search.ed4af04e.png"
+  },
+  {
+    "revision": "837c78fd1384d94abb995f03e9be55e0",
+    "url": "/static/media/shopping.837c78fd.jpg"
+  },
+  {
+    "revision": "40197ca08bba0b4a162960cfae978711",
+    "url": "/static/media/sydney-harbour.40197ca0.jpg"
+  },
+  {
+    "revision": "a38a61e957959f3feaef660b8c8db765",
+    "url": "/static/media/uow.a38a61e9.jpg"
+  },
+  {
+    "revision": "8e10c06c8e0816833c21f0df1a07212e",
+    "url": "/static/media/wheelchair.8e10c06c.jpg"
+  },
+  {
+    "revision": "72a08a9a59c097e75bd164801c325529",
+    "url": "/static/media/wildlife.72a08a9a.jpg"
+  },
+  {
+    "revision": "91a28907bcb0e6bb5c383525e5cc7d56",
+    "url": "/static/media/wine.91a28907.jpg"
+  },
+  {
+    "revision": "dc91695c1f63bce5d8aa2aaa20768377",
+    "url": "/static/media/wine_tasting.dc91695c.jpg"
+  }
+]);
